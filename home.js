b4463a7cdb853bc729e107cc8c29216de323b042
wolfmotherstudio <k.vanpelt87@gmail.com>
2022-01-28 19:06:42 -0600

    Update 1.01

--- home.js
+++ home.js
@@ -1,63 +1,16 @@
-
-$(function() {
-  // Vars
-  var pointsA = [],
-    pointsB = [],
-    $canvas = null,
-    canvas = null,
-    context = null,
-    vars = null,
-    points = 8,
-    viscosity = 20,
-    mouseDist = 70,
-    damping = 0.05,
-    showIndicators = false;
-    mouseX = 0,
-    mouseY = 0,
-    relMouseX = 0,
-    relMouseY = 0,
-    mouseLastX = 0,
-    mouseLastY = 0,
-    mouseDirectionX = 0,
-    mouseDirectionY = 0,
-    mouseSpeedX = 0,
-    mouseSpeedY = 0;
-
-  /**
-   * Get mouse direction
-   */
-  function mouseDirection(e) {
-    if (mouseX < e.pageX)
-      mouseDirectionX = 1;
-    else if (mouseX > e.pageX)
-      mouseDirectionX = -1;
-    else
-      mouseDirectionX = 0;
-
-    if (mouseY < e.pageY)
-      mouseDirectionY = 1;
-    else if (mouseY > e.pageY)
-      mouseDirectionY = -1;
-    else
-      mouseDirectionY = 0;
-
-    mouseX = e.pageX;
-    mouseY = e.pageY;
-
-    relMouseX = (mouseX - $canvas.offset().left);
-    relMouseY = (mouseY - $canvas.offset().top);
+$('.brand-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 5
+    }
   }
-  $(document).on('mousemove', mouseDirection);
-
-  /**
-   * Get mouse speed
-   */
-  function mouseSpeed() {
-    mouseSpeedX = mouseX - mouseLastX;
-    mouseSpeedY = mouseY - mouseLastY;
-
-    mouseLastX = mouseX;
-    mouseLastY = mouseY;
-
-    setTimeout(mouseSpeed, 50);
-  }
+})
